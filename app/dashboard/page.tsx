956a8ac6d98210ef717f9d01bad0570aab02b2b1
Benjamin Smith <bh2smith@users.noreply.github.com>
2024-06-03 08:24:26 +0200

    Chapter 9: Streaming

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,14 +2,27 @@ import { Card } from '@/app/ui/dashboard/cards';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
-import { fetchRevenue, fetchLatestInvoices, fetchCardData } from '@/app/lib/data';
- 
+import {
+  fetchRevenue,
+  fetchLatestInvoices,
+  fetchCardData,
+} from '@/app/lib/data';
+
 export default async function Page() {
-  const [revenue, latestInvoices, {totalPaidInvoices, totalPendingInvoices, numberOfInvoices, numberOfCustomers} ] = await Promise.all([
+  const [
+    revenue,
+    latestInvoices,
+    {
+      totalPaidInvoices,
+      totalPendingInvoices,
+      numberOfInvoices,
+      numberOfCustomers,
+    },
+  ] = await Promise.all([
     fetchRevenue(),
     fetchLatestInvoices(),
-    fetchCardData()
-  ]) 
+    fetchCardData(),
+  ]);
   // const revenue = await fetchRevenue();
   // const latestInvoices = await fetchLatestInvoices();
   // const {totalPaidInvoices, totalPendingInvoices, numberOfInvoices, numberOfCustomers} = await fetchCardData();
@@ -29,7 +42,7 @@ export default async function Page() {
         />
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
-        <RevenueChart revenue={revenue}  />
+        <RevenueChart revenue={revenue} />
         <LatestInvoices latestInvoices={latestInvoices} />
       </div>
     </main>
