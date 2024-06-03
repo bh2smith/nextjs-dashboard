f08cdc6579992c8af772cc48e2afdecdb2fb19f8
Benjamin Smith <bh2smith@users.noreply.github.com>
2024-06-03 10:11:54 +0200

    Chapter 11-13: Mutating Data (CrUD) & Error Handling (#5)

--- app/ui/invoices/pagination.tsx
+++ app/ui/invoices/pagination.tsx
@@ -10,7 +10,7 @@ export default function Pagination({ totalPages }: { totalPages: number }) {
   const pathname = usePathname();
   const searchParams = useSearchParams();
   const currentPage = Number(searchParams.get('page')) || 1;
-  
+
   const createPageURL = (pageNumber: number | string) => {
     const params = new URLSearchParams(searchParams);
     params.set('page', pageNumber.toString());
@@ -18,7 +18,6 @@ export default function Pagination({ totalPages }: { totalPages: number }) {
   };
   // NOTE: comment in this code when you get to this point in the course
   // const allPages = generatePagination(currentPage, totalPages);
-
 
   return (
     <>
